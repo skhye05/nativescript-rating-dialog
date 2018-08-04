3bcd19e2419eaf8e4dd4006f6153c042a14339d7
skhye05 <mayunga.j@gmail.com>
2018-08-04 19:48:47 +0200

    update test

--- demo/app/tests/tests.js
+++ demo/app/tests/tests.js
@@ -3,10 +3,6 @@ var ratingDialog = new RatingDialog();
 
 describe("greet function", function() {
     it("exists", function() {
-        expect(ratingDialog.greet).toBeDefined();
-    });
-
-    it("returns a string", function() {
-        expect(ratingDialog.greet()).toEqual("Hello, NS");
+        expect(ratingDialog.show).toBeDefined();
     });
 });
