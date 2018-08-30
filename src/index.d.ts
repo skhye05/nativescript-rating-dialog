43d44653b656fe4682805fbf173aeb580889bf9c
skhye05 <mayunga.j@gmail.com>
2018-08-30 13:31:05 +0200

    update

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,105 @@
-import { RatingDialogOption } from './rating-dialog.common';
+
 export declare class RatingDialog {
-    private ratingDialog;
-    constructor();
-    show(options: RatingDialogOption): void;
-    dismiss(): void;
+
+  constructor();
+
+  public show(option: RatingDialogOption): void;
+
+  public dismiss(): void;
+
 }
 
-export { RatingDialogOption } from "./rating-dialog.common";
+export declare interface RatingDialogOption {
+
+  icon?: string;
+
+  title: string;
+
+  textColor?: string;
+
+  backgroundColor?: string;
+
+  android?: AndroidRatingDialogOption;
+
+  ios?: IOSRatingDialogOption;
+}
+
+export declare interface AndroidRatingDialogOption {
+
+  session?: number;
+
+  threshold?: number;
+
+  positiveButtonTextColor?: string;
+
+  negativeButtonTextColor?: string;
+
+  positiveButtonText: string;
+
+  negativeButtonText?: string;
+
+  ratingBarColor?: string;
+
+  ratingBarBackgroundColor?: string;
+
+  playstoreUrl?: string;
+
+  feedbackForm?: AndroidRatingDialogFeedbackForm;
+
+  onDialogDismiss?: (rating) => void;
+
+  onThresholdCleared?: (ratingDialog: any, rating: number, thresholdCleared: boolean) => void;
+
+  onThresholdFailed?: (ratingDialog: any, rating: number, thresholdCleared: boolean) => void;
+
+  onRatingChanged?: (rating: number, thresholdCleared: boolean) => void;
+
+  onRatingBarFormSumbit?: (feedback: string) => void;
+
+}
+
+export declare interface AndroidRatingDialogFeedbackForm {
+
+  formTitle: string;
+
+  formSubmitText: string;
+
+  cancelText: string;
+
+  feedbackFormHint?: string;
+
+  feedBackTextColor?: string;
+}
+
+export declare interface IOSRatingDialogOption {
+
+  subtitle?: string;
+
+  onPositiveButtonColor?: string;
+
+  onNegativeButtonColor?: string;
+
+  onPositiveButtonText: string;
+
+  onNegativeButtonText?: string;
+
+  positiveText?: string;
+
+  negativeText?: string;
+
+  textColor?: string;
+
+  emptyStarImage?: string;
+
+  fullStarImage?: string;
+
+  dismissButtonColor?: string;
+
+  itunesId?: string;
+
+  onPositiveRate?: (rate: number) => void;
+
+  onNegativeRate?: (rate: number) => void;
+
+  onDismiss?: () => void;
+}
